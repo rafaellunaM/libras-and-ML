84c8368a6509707f989765ddc834a9e740f61b6a
rafael miguel luna <rafael.luna.cavalcanti@gmail.com>
2024-08-14 02:06:48 -0300

    save frames from video

--- capture_device.py
+++ capture_device.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import numpy as np
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
@@ -8,6 +9,7 @@ hands = mp_hands.Hands()
 
 pre_editado_path = 'abanar.mp4'
 capture_video = cv2.VideoCapture(pre_editado_path)
+frames_matrices = []
 
 while True:
     ret_preditado, frame_preditado = capture_video.read()
@@ -22,6 +24,8 @@ while True:
         for hand_landmarks in result_preditado.multi_hand_landmarks:
             mp_drawing.draw_landmarks(frame_preditado, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
+    frames_matrices.append(frame_preditado)
+
     cv2.imshow('Vídeo Pré-Editado', frame_preditado)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -29,3 +33,8 @@ while True:
 
 capture_video.release()
 cv2.destroyAllWindows()
+
+for i, frame_matrix in enumerate(frames_matrices):
+    filename = f'frame_{i:04d}.png'
+    cv2.imwrite(filename, frame_matrix)
+    print(f"Salvo: {filename}")
